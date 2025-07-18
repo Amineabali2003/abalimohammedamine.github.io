d6a3c20c39fe95e6e408ee7ca26ed8ee2b24a7d7
Amineabali2003 <abalimohammedamine@gmail.com>
2025-07-18 13:43:43 +0200

    Updates

---
+++ static/chunks/pages__app_5771e187._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_dist_4f3d469a._.js",
+  "static/chunks/node_modules_react_1cad9b0b._.js",
+  "static/chunks/node_modules_react-dom_cjs_react-dom_development_ab7e073c.js",
+  "static/chunks/node_modules_react-dom_f14d0471._.js",
+  "static/chunks/node_modules_3bfdc6a4._.js",
+  "static/chunks/[root-of-the-server]__fb6a6365._.js",
+  "static/chunks/styles_globals_scss_2d212ac6.css"
+],
+    source: "entry"
+});

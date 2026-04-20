dbd0b4c4b2f0fd994725302fda2fb1ca9cc2e7d6
Amineabali2003 <117599414+Amineabali2003@users.noreply.github.com>
2026-04-20 23:06:18 +0200

    clean: remove gh-pages, use Vercel only

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const isVercel = process.env.VERCEL === '1';
-
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: isProd && !isVercel ? '/abalimohammedamine.github.io' : '',
-  assetPrefix: isProd && !isVercel ? '/abalimohammedamine.github.io/' : '',
 };
 
 module.exports = nextConfig;
